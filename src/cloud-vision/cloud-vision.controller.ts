ad98def741127360f42de8d7a651ee51a16d502d
apple-yagi <applesryu@gmail.com>
2020-10-23 20:49:57 +0900

    update

--- src/cloud-vision/cloud-vision.controller.ts
+++ src/cloud-vision/cloud-vision.controller.ts
@@ -14,21 +14,25 @@ import { IUploadedFile } from 'types/file';
 
 @Controller('cloud-vision')
 export class CloudVisionController {
-  constructor(private readonly cloudVisionService: CloudVisionService) { }
+  constructor(private readonly cloudVisionService: CloudVisionService) {}
 
   @Post('detection')
   @UseInterceptors(FileInterceptor('file'))
   faceDetection(
     @UploadedFile() file: IUploadedFile,
     @Query('type') type: string,
-  ): Promise<AnnotateResult[]> {
+  ) {
     if (file === undefined) {
-      throw new HttpException({
-        status: HttpStatus.BAD_REQUEST,
-        error: "File not exist"
-      }, HttpStatus.BAD_REQUEST)
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'File not exist',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
     } else {
-      return this.cloudVisionService.detection(file, type);
+      return file;
+      // return this.cloudVisionService.detection(file, type);
     }
   }
 }
