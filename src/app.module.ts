b4ada87cd3df1631e59a628b0d69db86bdfa0603
apple-yagi <applesryu@gmail.com>
2020-10-24 15:13:39 +0900

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CloudVisionModule } from './cloud-vision/cloud-vision.module';
 
 @Module({
-  imports: [CloudVisionModule, CloudVisionModule, ConfigModule.forRoot()],
+  imports: [
+    CloudVisionModule,
+    CloudVisionModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/nestjs',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
