144fc45d56f35c1e279f7418acde2b96954f926a
apple-yagi <applesryu@gmail.com>
2020-10-28 09:23:20 +0900

    tasksのCRUDを作成

--- src/todos/tasks.controller.ts
+++ src/todos/tasks.controller.ts
@@ -1,19 +1,34 @@
-import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, ValidationPipe } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-todo.dto';
 import { Task } from './interfaces/task.interface';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private readonly tasksService: TasksService) {}
+  constructor(private readonly tasksService: TasksService) { }
 
   @Get()
   index(): Promise<Task[]> {
     return this.tasksService.findAll();
   }
 
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.findOne(id)
+  }
+
   @Post()
   create(@Body(ValidationPipe) createTask: CreateTaskDto) {
     return this.tasksService.create(createTask);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body(ValidationPipe) updateTask: CreateTaskDto) {
+    return this.tasksService.update(id, updateTask)
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.tasksService.delete(id)
   }
 }
--- src/todos/tasks.service.ts
+++ src/todos/tasks.service.ts
@@ -6,14 +6,46 @@ import { Task } from './interfaces/task.interface';
 
 @Injectable()
 export class TasksService {
-  constructor(@InjectModel('Task') private readonly taskModel: Model<Task>) {}
+  constructor(@InjectModel('Task') private readonly taskModel: Model<Task>) { }
 
   findAll(): Promise<Task[]> {
     return this.taskModel.find().exec();
   }
 
-  create(task: CreateTaskDto): Promise<Task> {
-    const createdTask = new this.taskModel(task);
-    return createdTask.save();
+  async findOne(id: string): Promise<Task> {
+    try {
+      const task = await this.taskModel.findById(id).exec()
+      return Promise.resolve(task)
+    } catch (e) {
+      return e
+    }
+  }
+
+  async create(task: CreateTaskDto): Promise<Task> {
+    const createTask = new this.taskModel(task);
+    try {
+      const createdTask = await createTask.save()
+      return Promise.resolve(createdTask)
+    } catch (e) {
+      return e
+    }
+  }
+
+  async update(id: string, updateTask: CreateTaskDto): Promise<Task> {
+    try {
+      const updatedTask = await this.taskModel.findByIdAndUpdate(id, updateTask)
+      return Promise.resolve(updatedTask)
+    } catch (e) {
+      return e
+    }
+  }
+
+  async delete(id: string): Promise<Task> {
+    try {
+      const deletedTask = await this.taskModel.findByIdAndRemove(id).exec()
+      return Promise.resolve(deletedTask)
+    } catch (e) {
+      return e
+    }
   }
 }
