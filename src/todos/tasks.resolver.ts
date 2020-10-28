dc6e4eea0538c74c5937aa84068869e635e98846
apple-yagi <applesryu@gmail.com>
2020-10-28 11:31:57 +0900

    taskのCRUD処理をGraphQLに対応

--- src/todos/tasks.resolver.ts
+++ src/todos/tasks.resolver.ts
@@ -5,11 +5,16 @@ import { TasksService } from './tasks.service';
 
 @Resolver('Tasks')
 export class TasksResolver {
-  constructor(private readonly tasksService: TasksService) {}
+  constructor(private readonly tasksService: TasksService) { }
 
   @Query(returns => [Task])
   async tasks(): Promise<Task[]> {
     return this.tasksService.findAll();
+  }
+
+  @Query(returns => Task)
+  async task(@Args('id', { type: () => String }) id: string): Promise<Task> {
+    return this.tasksService.findOne(id)
   }
 
   @Mutation(returns => Task)
@@ -22,5 +27,19 @@ export class TasksResolver {
       description: description,
     };
     return this.tasksService.create(createTask);
+  }
+
+  @Mutation(returns => Task)
+  async updateTask(@Args('id', { type: () => String }) id: string, @Args('title', { type: () => String }) title: string, @Args('description', { type: () => String }) description: string): Promise<Task> {
+    const updateTask: CreateTaskDto = {
+      title: title,
+      description: description
+    }
+    return this.tasksService.update(id, updateTask)
+  }
+
+  @Mutation(returns => Task)
+  async deleteTask(@Args('id', { type: () => String }) id: string): Promise<Task> {
+    return this.tasksService.delete(id)
   }
 }
