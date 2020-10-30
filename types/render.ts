33f0be939c42c5bc77d7b89ee02d58cc20ae52ba
apple-yagi <applesryu@gmail.com>
2020-10-30 11:56:23 +0900

    フォントサイズをレスポンシブ対応

--- types/render.ts
+++ types/render.ts
@@ -3,3 +3,13 @@ export interface HeadData {
   stylesheets: Array<string>,
   activePage: string,
 }
+
+export interface ContentData {
+  title: string,
+  img: {
+    src: string,
+    alt: string
+  },
+  href: string,
+  description: string
+}
