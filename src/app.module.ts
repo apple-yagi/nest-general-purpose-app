d57d5e5bf82c269112e37d88269f3b9bf7ba6f24
apple-yagi <applesryu@gmail.com>
2020-11-08 02:17:14 +0900

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,13 +18,16 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_HOST || 'localhost',
+      // host: process.env.MYSQL_HOST || 'localhost',
       port: 3306,
       username: process.env.MYSQL_USER || 'root',
       password: process.env.MYSQL_PASSWORD || 'root',
       database: process.env.MYSQL_DB || 'mydb',
       entities: [User],
       synchronize: true,
+      extra: {
+        socketPath: process.env.MYSQL_HOST,
+      },
     }),
     MongooseModule.forRoot(
       process.env.MONGODB_URI || 'mongodb://localhost:27017/nest',
