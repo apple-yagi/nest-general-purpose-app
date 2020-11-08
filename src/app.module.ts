95580b2654c5d81134865d736c73ba0fdcb87a5d
apple-yagi <applesryu@gmail.com>
2020-11-08 18:15:24 +0900

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,13 +18,14 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      // host: process.env.MYSQL_HOST || 'localhost',
+      host: process.env.MYSQL_HOST || 'localhost',
       port: 3306,
       username: process.env.MYSQL_USER || 'root',
       password: process.env.MYSQL_PASSWORD || 'root',
       database: process.env.MYSQL_DB || 'mydb',
       entities: [User],
-      synchronize: true,
+      synchronize: false,
+      logging: process.env.NODE_ENV === 'development' ? true : false,
       extra: {
         socketPath: process.env.MYSQL_HOST,
       },
