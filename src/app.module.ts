b40626ec19da23e97925c718fe7486cc261c98fb
apple-yagi <applesryu@gmail.com>
2020-11-22 23:39:33 +0900

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,25 +21,6 @@ import { EventsModule } from './events/events.module';
     CloudVisionModule,
     CloudVisionModule,
     ConfigModule.forRoot(),
-    process.env.NODE_ENV === 'development'
-      ? TypeOrmModule.forRoot({
-          type: 'mysql',
-          host: process.env.MYSQL_HOST || 'localhost',
-          port: 3306,
-          username: process.env.MYSQL_USER || 'root',
-          password: process.env.MYSQL_PASSWORD || 'root',
-          database: process.env.MYSQL_DB || 'mydb',
-          entities: [User, Item],
-          synchronize: true,
-          logging: process.env.NODE_ENV === 'development' ? true : false,
-          extra:
-            process.env.NODE_ENV === 'development'
-              ? {}
-              : {
-                  socketPath: process.env.MYSQL_HOST,
-                },
-        })
-      : null,
     MongooseModule.forRoot(
       process.env.MONGODB_URI || 'mongodb://localhost:27017/nest',
     ),
@@ -52,10 +33,27 @@ import { EventsModule } from './events/events.module';
     }),
     TasksModule,
     LabelDetectionResultsModule,
-    process.env.NODE_ENV === 'development' ? UsersModule : null,
-    process.env.NODE_ENV === 'development' ? AuthModule : null,
-    process.env.NODE_ENV === 'development' ? ItemsModule : null,
-    process.env.NODE_ENV === 'development' ? EventsModule : null,
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.MYSQL_HOST || 'localhost',
+    //   port: 3306,
+    //   username: process.env.MYSQL_USER || 'root',
+    //   password: process.env.MYSQL_PASSWORD || 'root',
+    //   database: process.env.MYSQL_DB || 'mydb',
+    //   entities: [User, Item],
+    //   synchronize: true,
+    //   logging: process.env.NODE_ENV === 'development' ? true : false,
+    //   extra:
+    //     process.env.NODE_ENV === 'development'
+    //       ? {}
+    //       : {
+    //           socketPath: process.env.MYSQL_HOST,
+    //         },
+    // }),
+    // UsersModule,
+    // AuthModule,
+    // ItemsModule,
+    // EventsModule,
   ],
   controllers: [AppController, ItemsController],
   providers: [AppService],
